18b874158b362c41be1885c9249078f197c136db
Facundo Uriel Gutierrez <tdetierrez@gmail.com>
2025-10-28 15:52:19 -0300

    Solucionando error de pull

--- Front-end/hamilton-bar/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-function Footer(){
-    return (
-        <footer className="bg-amber-900 text-amber-100 py-8 text-center border-t-4 border-orange-500">© 2025 Hamilton Bar. Todos los derechos reservados.</footer>
-    )
-}
-
-export default Footer
